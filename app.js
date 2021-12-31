9014c885ef043821127fde06724a2dffe8a535c7
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-12-31 14:33:26 -0700

    added user component

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import React from "react";
 import axios from "axios";
 import "./styles.css";
 
-const User = ({ img, name }) => (
-  <>
-    <div>{name}</div>
-    <img src={img} alt="avatar "></img>
-  </>
+const User = (props) => (
+  <div>
+    <div>{props.name}</div>
+    <div></div>
+    <img src={props.img} alt="avatar "></img>
+    <div>Public Repos: {props.repos}</div>
+    <div>Public Gists: {props.gists}</div>
+    <div>Followers: {props.followers}</div>
+    <div>Score: {props.score}</div>
+  </div>
 );
 
 class App extends React.Component {
@@ -43,15 +48,24 @@ class App extends React.Component {
     const hasUser = !this.state.isLoading && this.state.users;
     return (
       <div className="App">
+        <h1>Github Profile Battle</h1>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.searchTerm} />
         </form>
         {this.state.hasError && <div>User Not Found</div>}
         {this.state.isLoading && <div>loading</div>}
         {hasUser && (
-          <div>
+          <div className="user">
             {this.state.users.map((user) => (
-              <User img={user.avatar_url} name={user.name} key={user.id} />
+              <User
+                img={user.avatar_url}
+                name={user.name}
+                key={user.id}
+                repos={user.public_repos}
+                gists={user.public_gists}
+                followers={user.followers}
+                score={user.public_repos + user.public_gists + user.followers}
+              />
             ))}
           </div>
         )}
